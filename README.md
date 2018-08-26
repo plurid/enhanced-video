a53b4882aed29207aebf62b294413a3c682e625d
Jan Cavel <caveljan@gmail.com>
2018-08-27 00:49:16 +0300

    Setup: initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Video Text Select
+
+
+## Concept
+
+Each frame of a video is analysed and established if it contains text. If it does, the timestamp of the video, and the location of the text (x, y coordinates) are registered and on top of the video is placed a transparent div, cotaining exactly the text from the video. The text from the video behaves like it is truly selectable.
+
+
+<p align="center">
+    <img src="https://raw.githubusercontent.com/caveljan/video-text-select/master/docs/images/1.png" height="500px">
+</p>
+
+<p align="center">
+    <img src="https://raw.githubusercontent.com/caveljan/video-text-select/master/docs/images/2.png" height="500px">
+</p>
+
+<p align="center">
+    <img src="https://raw.githubusercontent.com/caveljan/video-text-select/master/docs/images/3.png" height="500px">
+</p>
---
+++ webpack.config.js
@@ -0,0 +1,53 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+
+
+const miniCssExtract = new MiniCssExtractPlugin({
+    filename: "../css/video-text-select.css",
+    disable: process.env.NODE_ENV === "development"
+});
+const path = require('path');
+
+
+module.exports = {
+    mode: 'development',
+    // mode: 'production',
+    entry: './app.js',
+    output: {
+        filename: 'video-text-select.js',
+        path: path.resolve(__dirname, 'video-text-select/js')
+    },
+    optimization: {
+        minimizer: [
+            new UglifyJsPlugin({
+                cache: true,
+                parallel: true,
+                sourceMap: true
+            }),
+            new OptimizeCSSAssetsPlugin({})
+        ]
+    },
+    plugins: [
+        miniCssExtract
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.scss$/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader',
+                ],
+            },
+            {
+                test: /\.ts$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "awesome-typescript-loader"
+                }
+            }
+        ]
+    }
+};
