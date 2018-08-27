b92ced16b2b3522480229f93a35ec444eee82d19
Jan Cavel <caveljan@gmail.com>
2018-08-27 23:36:20 +0300

    Dev: test data

---
+++ src/ts/source/dev.ts
@@ -0,0 +1,11 @@
+/**
+ * Video is sent to vts.deview.plurid.com
+ *
+ * An object is generated (./data/text.json)
+ * and received
+ *
+ * The received text is 'typeset' in the video-text-select container
+ * As the video is playing the appropiate text is set to display block
+ * (from display none) while keeping the text color transparent.
+ *
+ */
