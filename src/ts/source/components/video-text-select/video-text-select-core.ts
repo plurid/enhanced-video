931aa822227d9af800c1865ddee2c66ec8eea0f4
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2018-08-28 10:08:38 +0300

    Dev: load json vts data

--- src/ts/source/components/video-text-select/video-text-select-core.ts
+++ src/ts/source/components/video-text-select/video-text-select-core.ts
@@ -2,20 +2,30 @@ import { HTMLVideoTextSelectElement } from './video-text-select-define';
 
 
 
-export function setVideo(video: HTMLVideoTextSelectElement) {
-    console.log('autoplay', video.autoplay);
-    console.log('controls', video.controls);
-    console.log('height', video.height);
-    console.log('loop', video.loop);
-    console.log('muted', video.muted);
-    console.log('poster', video.poster);
-    console.log('pregenerate', video.pregenerate);
-    console.log('preload', video.preload);
-    console.log('source', video.source);
-    console.log('sources', video.sources);
-    console.log('width', video.width);
+interface IVideoText {
+    id: string,
+    begin: number,
+    end: number,
+    xCoord: number,
+    yCoord: number,
+    perspective: string,
+    rotation: string,
+    skew: string,
+    fontSize: number,
+    letterSpacing: number,
+    textTransform: string,
+    textContent: string
+}
 
-    let videoEl: HTMLVideoElement = document.createElement('video');
+
+interface IVTSData {
+    id: string,
+    videoText: IVideoText[]
+}
+
+
+export function setVideo(video: HTMLVideoTextSelectElement) {
+    const videoEl: HTMLVideoElement = document.createElement('video');
 
     videoEl.autoplay = video.autoplay;
     // videoEl.controls = video.controls;
@@ -28,4 +38,72 @@ export function setVideo(video: HTMLVideoTextSelectElement) {
     videoEl.width = parseInt(video.width);
 
     video.appendChild(videoEl);
+
+
+    // Dummy load data and based on it set the video text.
+    loadJSON('./data/text.json', (vtsData: IVTSData) => {
+        setVideoText(video, vtsData);
+    });
+}
+
+
+
+/**
+ * Utility function to load dummy data
+ * which will be received from the server.
+ *
+ * @param path {string}
+ * @param callback {Function}
+ */
+function loadJSON(path: string, callback: Function) {
+    let xobj = new XMLHttpRequest();
+    xobj.overrideMimeType("application/json");
+
+    xobj.open('GET', path, true);
+    xobj.onreadystatechange = () => {
+        if (xobj.readyState == 4 && xobj.status == 200) {
+            callback( JSON.parse(xobj.responseText) );
+        }
+    };
+
+    xobj.send(null);
+}
+
+
+
+function setVideoText(video: HTMLVideoTextSelectElement, vtsData: IVTSData) {
+    console.log('autoplay', video.autoplay);
+    console.log('controls', video.controls);
+    console.log('height', video.height);
+    console.log('loop', video.loop);
+    console.log('muted', video.muted);
+    console.log('poster', video.poster);
+    console.log('pregenerate', video.pregenerate);
+    console.log('preload', video.preload);
+    console.log('source', video.source);
+    console.log('sources', video.sources);
+    console.log('width', video.width);
+
+    const videoEl = video.getElementsByTagName('video')[0];
+
+    console.log(video);
+    console.log(videoEl);
+
+    video.id = vtsData.id;
+
+    let videoText = document.createElement('div');
+
+    vtsData.videoText.map((videoTextEl: IVideoText) => {
+
+        let div = document.createElement('div');
+
+        div.innerText = videoTextEl.textContent;
+
+        videoText.appendChild(div);
+        // console.log(videoTextEl.id);
+    });
+
+    video.appendChild(videoText);
+
+    console.log('vtsData', vtsData);
 }
