6586e06bd8cdba6a2559e64a904e728c9aa7a005
Jan Cavel <caveljan@gmail.com>
2018-08-29 22:47:39 +0300

    Dev: video controls

--- src/ts/source/components/components.ts
+++ src/ts/source/components/components.ts
@@ -1,3 +1,4 @@
 import './video-text-select/video-text-select-define.ts';
 import './video-text/video-text-define.ts';
 import './video-select/video-select-define.ts';
+import './video-text-select-controls/video-text-select-controls-define.ts';
