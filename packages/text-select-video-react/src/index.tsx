29574e92c89fa5211fa30dc6fcfd9426019719ab
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2019-06-07 11:15:07 +0300

    setup: packages

--- packages/text-select-video-react/src/index.tsx
+++ packages/text-select-video-react/src/index.tsx
@@ -2,4 +2,11 @@ import React, { Component } from 'react';
 
 
 
-export default class TextSelectVideo extends Component {}
+export default class TextSelectVideo extends Component {
+
+    render() {
+        return (
+            <div>Video</div>
+        )
+    }
+}
