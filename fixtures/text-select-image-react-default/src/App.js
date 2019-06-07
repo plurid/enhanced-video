898a269c003c1d21d8bf6c2ed9c3d642c41a6cde
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2019-06-07 10:24:30 +0300

    text-select-video-react > setup: packages

---
+++ fixtures/text-select-image-react-default/src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+// import video from './assets/video.mov';
+
+import TextSelectVideo from '@plurid/text-select-video-react';
+
+
+
+class App extends Component {
+    render () {
+        return (
+            <div className="app">
+                <div
+                    style={ {width: 800, margin: '150px auto'} }
+                >
+                    <TextSelectVideo
+                        theme="night"
+                        // src={video}
+                        controls={true}
+                        // getTextOnLoad={true}
+                        apiEndpoint="http://192.168.1.2:3360/graphql"
+                        // apiEndpoint="https://api.plurid.com/graphql"
+                    />
+                </div>
+            </div>
+        );
+    }
+}
+
+
+export default App;
