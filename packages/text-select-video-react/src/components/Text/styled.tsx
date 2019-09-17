fd9239fcfa58e866c1a6c0586dc7b20938bfa3e0
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2019-09-17 20:16:08 +0300

    text-select-video-react > dev: files re-arrangement

---
+++ packages/text-select-video-react/src/components/Text/styled.tsx
@@ -0,0 +1,23 @@
+import styled from 'styled-components';
+
+
+
+export const StyledText = styled.div`
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    /* background: red; */
+`;
+
+
+// export const StyledSelectVideo = styled.div`
+//     position: absolute;
+//     top: 0;
+//     left: 0;
+//     /* background: red; */
+//     /* opacity: 0.3; */
+//     /* width: 200px; */
+//     /* height: 200px; */
+// `;
