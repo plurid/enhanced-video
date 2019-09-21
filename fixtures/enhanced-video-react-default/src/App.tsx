796a0bc4865e53581622e9233153049aebbd8b64
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2019-09-21 14:55:25 +0300

    enhanced-video-react > fixture: package

--- fixtures/enhanced-video-react-default/src/App.tsx
+++ fixtures/enhanced-video-react-default/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import EnhancedVideo from '@plurid/enhanced-video-react';
 
 import video from './assets/video.mov';
+import longVideo from './assets/long-video.mp4';
 
 
 
@@ -48,9 +49,9 @@ const App: React.FC = () => {
                 style={ {width: 900, margin: '150px auto'} }
             >
                 <EnhancedVideo
-                    src={video}
+                    src={longVideo}
                     type="video/mp4"
-                    // theme="dusk"
+                    theme="night"
                     // qualitySources={qualitySources}
                     // about={false}
                     // apiEndpoint="http://192.168.1.2:33600/graphql"
