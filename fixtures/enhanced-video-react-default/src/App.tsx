3c4f6dfd112cf9521f4c14e66db1715fe099bfde
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2019-09-21 14:43:30 +0300

    enhanced-video-react > setup: package

--- fixtures/enhanced-video-react-default/src/App.tsx
+++ fixtures/enhanced-video-react-default/src/App.tsx
@@ -5,40 +5,40 @@ import video from './assets/video.mov';
 
 
 
-const qualitySources = [
-    {
-        quality: '144p',
-        src: '/path/to/144p',
-    },
-    {
-        quality: '240p',
-        src: '/path/to/240p',
-    },
-    {
-        quality: '360p',
-        src: '/path/to/360',
-    },
-    {
-        quality: '480p',
-        src: '/path/to/480p',
-    },
-    {
-        quality: '720p',
-        src: '/path/to/720p',
-    },
-    {
-        quality: '1080p',
-        src: '/path/to/1080p',
-    },
-    {
-        quality: '1440p',
-        src: '/path/to/1440p',
-    },
-    {
-        quality: '2160p',
-        src: '/path/to/2160p',
-    },
-];
+// const qualitySources = [
+//     {
+//         quality: '144p',
+//         src: '/path/to/144p',
+//     },
+//     {
+//         quality: '240p',
+//         src: '/path/to/240p',
+//     },
+//     {
+//         quality: '360p',
+//         src: '/path/to/360',
+//     },
+//     {
+//         quality: '480p',
+//         src: '/path/to/480p',
+//     },
+//     {
+//         quality: '720p',
+//         src: '/path/to/720p',
+//     },
+//     {
+//         quality: '1080p',
+//         src: '/path/to/1080p',
+//     },
+//     {
+//         quality: '1440p',
+//         src: '/path/to/1440p',
+//     },
+//     {
+//         quality: '2160p',
+//         src: '/path/to/2160p',
+//     },
+// ];
 
 
 const App: React.FC = () => {
@@ -50,8 +50,8 @@ const App: React.FC = () => {
                 <EnhancedVideo
                     src={video}
                     type="video/mp4"
-                    theme="dusk"
-                    qualitySources={qualitySources}
+                    // theme="dusk"
+                    // qualitySources={qualitySources}
                     // about={false}
                     // apiEndpoint="http://192.168.1.2:33600/graphql"
                     // apiEndpoint="https://api.plurid.dev/graphql"
