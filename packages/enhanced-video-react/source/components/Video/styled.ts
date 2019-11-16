315b17e1c345c9d9114ce4a698fe4645580e0f9c
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2019-11-16 20:56:02 +0200

    enhanced-video-react > dev: Video, EnhancedVideo

--- packages/enhanced-video-react/source/components/Video/styled.ts
+++ packages/enhanced-video-react/source/components/Video/styled.ts
@@ -3,8 +3,11 @@ import styled from 'styled-components';
 
 
 export const StyledVideo: any = styled.div`
+    width: 100%;
+
     video {
         outline: none;
         display: block;
+        width: 100%;
     }
 `;
