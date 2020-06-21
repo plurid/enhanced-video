c1fab9bd93c7b003a3dede6cebb65c7cda748ccf
Jan Cavel <caveljan@gmail.com>
2020-06-21 19:29:17 +0300

    enhanced-video-react > setup: package

---
+++ packages/enhanced-video-react/source/containers/EnhancedVideo/__stories__/index.stories.tsx
@@ -0,0 +1,60 @@
+import React, {
+    useState,
+    useEffect,
+} from 'react';
+
+// import themes from '@plurid/plurid-themes';
+
+import { storiesOf } from '@storybook/react';
+// import { action } from '@storybook/addon-actions';
+
+import {
+    withKnobs,
+    boolean,
+    // number,
+    select,
+    text,
+} from '@storybook/addon-knobs';
+
+import EnhancedVideo from '../';
+
+
+
+// const actions = {
+//     // atClick: action('atClick'),
+// };
+
+// const themeLabel = 'Theme';
+// const computeThemeOptions = () => {
+//     const options: any = {};
+//     for (const key of Object.keys(themes)) {
+//         options[key] = key;
+//     }
+//     return options;
+// }
+// const themeOptions = computeThemeOptions();
+// const defaultThemeValue = 'plurid';
+
+storiesOf(
+    'EnhancedImage',
+    module,
+)
+.addDecorator(withKnobs)
+.add('basic', () => {
+    // const theme = select(themeLabel, themeOptions, defaultThemeValue);
+
+
+    return (
+        <div
+            style={{
+                padding: '4rem',
+                width: '800px',
+            }}
+        >
+            <EnhancedVideo
+                src="/assets/video.mov"
+                type="mov"
+            />
+        </div>
+    );
+});
