459b470e9059a63c27d290bde6de91ad3bc4b9da
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-06-22 11:39:12 +0300

    enhanced-video-react > dev: Video

--- packages/enhanced-video-react/source/components/Video/styled.ts
+++ packages/enhanced-video-react/source/components/Video/styled.ts
@@ -4,6 +4,9 @@ import styled from 'styled-components';
 
 export const StyledVideo: any = styled.div`
     width: 100%;
+    height: 100%;
+    display: grid;
+    place-content: center;
 
     video {
         outline: none;
